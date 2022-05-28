45d0b3ba9a5728dd7074ed52753f8406f88b479c
ariadna-aldeguer <gitD45120tw>
2022-05-28 17:51:19 +0200

    formulario-web-crud-spring

--- formulario-spring/src/main/java/com/example/demo/FormularioSpringApplication.java
+++ formulario-spring/src/main/java/com/example/demo/FormularioSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmazonApiRestApplication {
+public class FormularioSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AmazonApiRestApplication.class, args);
+		SpringApplication.run(FormularioSpringApplication.class, args);
 	}
 
 }
